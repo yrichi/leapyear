2dd7a7a186fb1efdf257bc1664c44f624cd63426
yrichi <yrichi59@gmail.com>
2024-12-20 20:53:18 +0100

    init kata

---
+++ src/main/java/com/kata/leapyear/LeapYearApplication.java
@@ -0,0 +1,13 @@
+package com.kata.leapyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapYearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeapYearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kata/leapyear/LeapYearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.leapyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapYearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
