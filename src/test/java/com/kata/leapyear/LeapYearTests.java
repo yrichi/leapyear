cb50aacf78fee3445d6410f4cbafa2e051c3ef52
yrichi <yrichi59@gmail.com>
2024-12-20 22:08:43 +0100

    first test : is leap year

---
+++ src/test/java/com/kata/leapyear/LeapYearTests.java
@@ -0,0 +1,32 @@
+package com.kata.leapyear;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class LeapYearTests {
+
+//    ## Règles
+//1. Une année est bissextile si elle est divisible par 4.
+//            2. Une année n’est pas bissextile si elle est divisible par 100 mais pas par 400.
+//            3. Les autres années ne sont pas bissextiles.
+//
+//### Exemple de sortie
+//    Pour les années suivantes :
+//            - 1997 : non bissextile.
+//            - 1996 : bissextile.
+//- 1900 : non bissextile.
+//            - 2000 : bissextile.
+
+
+    @Test
+    public void should_return_true_when_year_is_not_divisible_by_4() {
+        // GIVEN
+        LeapYear leapYear = new LeapYear();
+        // WHEN
+        boolean result = leapYear.isLeapYear(1997);
+        // THEN
+        Assertions.assertThat(result).isTrue();
+    }
+
+}
